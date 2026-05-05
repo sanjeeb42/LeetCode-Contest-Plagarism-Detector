4e6edec0f76add4b46e3ba8d87b69f0201dadb8f
Sanjeeb <raisanjeeb42@gmail.com>
2026-05-05 23:00:33 +0530

    removed unwanted replay debuggers

--- backend/check_html.py
+++
@@ -1,6 +0,0 @@
-from curl_cffi import requests
-
-response = requests.get("https://leetcode.com/contest/weekly-contest-431/submissions/detail/1501309832/", impersonate="chrome")
-with open("test_html.txt", "w") as f:
-    f.write(response.text)
-print(len(response.text))
